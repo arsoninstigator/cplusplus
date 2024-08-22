3c62536b0310cd9cd197b9d39cca1d79e310f67c
anne <anoushkabh@outlook.com>
2024-08-23 00:21:53 +0530

    update main.cpp

--- snake-game/main.cpp
+++ snake-game/main.cpp
@@ -93,3 +93,71 @@ void Input()
         }
     }
 }
+void Logic()
+{
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    int prev2X, prev2Y;
+    tailX[0] = x;
+    tailY[0] = y;
+    for (int i = 1; i < nTail; i++)
+    {
+        prev2X = tailX[i];
+        prev2Y = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = prev2X;
+        prevY = prev2Y;
+    }
+    switch (dir)
+    {
+        case LEFT:
+            x--;
+            break;
+        case RIGHT:
+            x++;
+            break;
+        case UP:
+            y--;
+            break;
+        case DOWN:
+            y++;
+            break;
+        default:
+            break;
+    }
+    if (x > width || x < 0 || y > height || y < 0)
+      gameOver = true;
+    //if (x >= width) x = 0; else if (x < 0) x = width - 1;
+    //if (y >= height) y = 0; else if (y < 0) y = height - 1;
+
+    for (int i = 0; i < nTail; i++)
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = true;
+
+    if (x == fruitX && y == fruitY)
+    {
+        srand(time(0)); // Random seed value for rand based on time
+        score += 10;
+        fruitX = rand() % width;
+        fruitY = rand() % height;
+        nTail++;
+    }
+}
+int main()
+{
+    //Will make cout much faster
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    
+    system("MODE con cols=24 lines=25");
+    Setup();
+    while (!gameOver)
+    {
+        Draw();
+        Input();
+        Logic();
+        Sleep(50); //sleep(10);
+    }
+    return 0;
+}
